8360642d160c63614c05da03219818cb435b864b
Dirty-Dog <anurrag116@gmail.com>
2023-04-07 14:21:43 +0530

    bullet logics

--- Ace Of Space/Assets/Scripts/BulletLauncher.cs
+++ Ace Of Space/Assets/Scripts/BulletLauncher.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Bullet
+{
+    public string Type;
+    public GameObject prefab;
+
+}
+
+
+
 public class BulletLauncher : MonoBehaviour
 {
+    public Bullet[] bullets;
     // Start is called before the first frame update
     void Start()
     {
